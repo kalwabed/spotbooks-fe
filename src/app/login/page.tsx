fc139f753f44835a14afbc8548ad223f09a3796e
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-21 01:58:45 +0700

    sindre patience

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,40 @@
 'use client'
 
-import { Button, Container, Flex, FormControl, FormLabel, Heading, Input, useStatStyles } from "@chakra-ui/react";
+import { tokenAtom } from "@/store/auth";
+import { Button, Container, Flex, FormControl, FormLabel, Heading, Input, useStatStyles, useToast } from "@chakra-ui/react";
+import { useSetAtom } from "jotai";
+import ky from 'ky'
 import React from "react";
 
 export default function LoginPage() {
   const username = React.useRef('')
+  const toast = useToast()
+  const setToken = useSetAtom(tokenAtom)
+
   const handleOnSubmit = async (e) => {
     e.preventDefault()
-    console.log(username.current.value)
+
+    try {
+      const user = await ky.post('http://localhost:8080/members/login', {
+        json: {
+          username: username.current.value
+        }
+      }).json<{ id: string; username: string; point: number }>()
+
+      toast({
+        title: 'You have been sign in',
+        description: 'Happy shopping!',
+        status: 'success'
+      })
+      setToken(user.id)
+    } catch (error) {
+      console.error(error)
+      toast({
+        title: 'Failed to login',
+        description: 'Username is not found!',
+        status: 'error'
+      })
+    }
   }
 
   return (
